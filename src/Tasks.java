04ac0edf3b3720a50e4dcff4c7f2d556a50907b5
AlexShmukler <alexe0610@gmail.com>
2017-09-21 17:56:17 +0300

    Commit

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,54 +1,39 @@
 
-import java.util.Scanner;
-public class First {
-    public static void main(String[] args) {
-    }
-}
 class Tasks {
-    FirstTaskAnswer BeginTask(int r) {
+    public FirstTaskAnswer beginTask(int r) {
         FirstTaskAnswer answer = new FirstTaskAnswer();
-        if(r<=0) {
-           throw new AssertionError("Wrong input (First Task)");
-        }
-        answer.S = 3.14 * r * r;
-        answer.L = 2 * 3.14 * r;
+        assert r >= 0: "Wrong input (First Task)";
+        answer.s = 3.14 * r * r;
+        answer.l = 2 * 3.14 * r;
         return answer;
     }
 
-    SecondTaskAnswer IntegerTask(int value) {
+    public SecondTaskAnswer integerTask(int value) {
         SecondTaskAnswer answer = new SecondTaskAnswer();
-        int First,Second;
-        if ((value > 99) || (value < 10)) {
-            throw new AssertionError("Wrong input (Integer Task)");
-        }
-        First = value / 10;
-        Second = value % 10;
-        answer.First = First + Second;
-        answer.Second = First*Second;
+        int first,second;
+        assert ((value < 100) || (value > 9)): "Wrong input (Integer Task)";
+        first = value / 10;
+        second = value % 10;
+        answer.first = first + second;
+        answer.second = first*second;
         return answer;
     }
 
-    boolean BooleanTask(int value1, int value2, int value3) {
-        if ((value1 < value2) && (value2 < value3)) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean booleanTask(int value1, int value2, int value3) {
+        return ((value1 < value2) && (value2 < value3));
     }
 
-    int IfTask(int value1, int value2) {
-        if(value1 == value2) {
-            throw new AssertionError("Wrong Input (If Task)");
-        }
+    public int ifTask(int value1, int value2) {
+        assert value1 != value2: "Wrong input (If Task)";
         if (value1 > value2) {
             return 2;
         } else {
             return 1;
         }
     }
-    double SwitchTask(int type, double weight){
+    public double switchTask(int type, double weight){
 
-        double answer = 0;
+        double answer;
         switch (type){
             case 1:
                 answer = weight;
@@ -70,27 +55,21 @@ class Tasks {
         }
         return answer;
     }
-    int ForTask(int value1, int value2){
-        if(value2 <= value1){
-            throw new AssertionError("Wrong input (For Task)");
-        }
+    public int forTask(int value1, int value2){
+        assert value2 >= value1: "Wrong input (For Task)";
         return (value1+value2)*(value2-value1+1)/2;
     }
-    int WhileTask(int N){
+    public int whileTask(int N){
         int k=0;
-        if(N<0){
-            throw new AssertionError("Wrong input (While Task)");
-        }
+        assert N > 0: "Wrong input (While Task)";
         while( (k*k) < N) {
          k++;
         }
         return k;
     }
-    int ArrayTask(int[] mas){
+    public int arrayTask(int[] mas){
         int n=mas.length;
-        if(n<2){
-            throw new AssertionError("Wrong input (Array Task)");
-        }
+        assert n>1: "Wrong input (Array Task)";
         for(int i=2;i<n;i++){
             if( (mas[i]-mas[i-1]) != (mas[i-1]-mas[i-2]) ){
                 return 0;
@@ -98,11 +77,9 @@ class Tasks {
         }
         return mas[1]-mas[0];
     }
-    int[][] MatrixTask(int[][] result, int n, int m){
-        int matrix[][] = new int[1][1];
-        if(n<1 || m<1){
-            return matrix;
-        }
+    public int[][] matrixTask(int[][] result){
+        int n = result.length, m = result[0].length;
+        assert (n > 0 || m > 0): "Wrong input (Matrix Task)";
         int k=0,line=0;
         for(int j=m-1;j>0;j--){
             if(k==n){
@@ -129,15 +106,15 @@ class Tasks {
 
 }
 class FirstTaskAnswer {
-    double S;
-    double L;
-    FirstTaskAnswer(double S,double L){
-        this.S = S;
-        this.L = L;
+    double s;
+    double l;
+    FirstTaskAnswer(double s,double l){
+        this.s = s;
+        this.l = l;
     }
     FirstTaskAnswer(){
-        this.S = 0;
-        this.L = 0;
+        this.s = 0;
+        this.l = 0;
     }
     @Override
     public boolean equals(Object obj){
@@ -148,19 +125,19 @@ class FirstTaskAnswer {
             return false;
 
         FirstTaskAnswer temp = (FirstTaskAnswer)obj;
-        return ( (this.S == temp.S) || (this.L == temp.L) );
+        return ( (this.s == temp.s) || (this.l == temp.l) );
     }
 }
 
 class SecondTaskAnswer {
-    int First, Second;
+    int first, second;
     SecondTaskAnswer(int first,int second){
-        this.First = first;
-        this.Second = second;
+        this.first = first;
+        this.second = second;
     }
     SecondTaskAnswer(){
-        this.First = 0;
-        this.Second = 0;
+        this.first = 0;
+        this.second = 0;
     }
     @Override
     public boolean equals(Object obj){
@@ -171,6 +148,6 @@ class SecondTaskAnswer {
             return false;
 
         SecondTaskAnswer temp = (SecondTaskAnswer)obj;
-        return ( (this.First == temp.First) || (this.Second == temp.Second) );
+        return ( (this.first == temp.first) || (this.second == temp.second) );
     }
 }
--- src/TasksTest.java
+++ src/TasksTest.java
@@ -12,54 +12,54 @@ public class TasksTest {
     public void testBeginTask() {
         //S = 3.14 * r * r
         //L = 2 * 3.14 * r
-        assertEquals(new Tasks().BeginTask(1),new FirstTaskAnswer(3.14, 6.28));
-        assertEquals(new Tasks().BeginTask(4),new FirstTaskAnswer(50.24,25.12));
+        assertEquals(new Tasks().beginTask(1),new FirstTaskAnswer(3.14, 6.28));
+        assertEquals(new Tasks().beginTask(4),new FirstTaskAnswer(50.24,25.12));
 
     }
     @Test(expected = AssertionError.class)
     public void WrongInputBeginTask() {
         //S = 3.14 * r * r
         //L = 2 * 3.14 * r
-        assertEquals(new Tasks().BeginTask(-3),new FirstTaskAnswer(3.14, 6.28));
-        assertEquals(new Tasks().BeginTask(0),new FirstTaskAnswer(50.24,25.12));
+        assertEquals(new Tasks().beginTask(-3),new FirstTaskAnswer(3.14, 6.28));
+        assertEquals(new Tasks().beginTask(0),new FirstTaskAnswer(50.24,25.12));
 
     }
     @Test
     public void testIntegerTask()  {
         //First = value/10
         //Second = value%10
-        assertEquals(new Tasks().IntegerTask(93),new SecondTaskAnswer(12,27));
-        assertEquals(new Tasks().IntegerTask(11),new SecondTaskAnswer(2,1));
+        assertEquals(new Tasks().integerTask(93),new SecondTaskAnswer(12,27));
+        assertEquals(new Tasks().integerTask(11),new SecondTaskAnswer(2,1));
 
     }
 
     @Test(expected = AssertionError.class)
     public void WrongInputIntegerTask() {
-        assertEquals(new Tasks().IntegerTask(12),new SecondTaskAnswer(1,2));
-        assertEquals(new Tasks().IntegerTask(321),new SecondTaskAnswer(0,0));
+        assertEquals(new Tasks().integerTask(12),new SecondTaskAnswer(1,2));
+        assertEquals(new Tasks().integerTask(321),new SecondTaskAnswer(0,0));
 
     }
 
     @Test
     public void testBooleanTask() {
         //value1 < value2 < value3
-        assertEquals(new Tasks().BooleanTask(1,2,3),true);
-        assertEquals(new Tasks().BooleanTask(4,2,3),false);
-        assertEquals(new Tasks().BooleanTask(1,2,-1),false);
+        assertEquals(new Tasks().booleanTask(1,2,3),true);
+        assertEquals(new Tasks().booleanTask(4,2,3),false);
+        assertEquals(new Tasks().booleanTask(1,2,-1),false);
 
     }
 
     @Test
     public void testIfTask() {
         //the ordinal number of the smaller number
-        assertEquals(new Tasks().IfTask(1,2),1);
-        assertEquals(new Tasks().IfTask(5,2),2);
+        assertEquals(new Tasks().ifTask(1,2),1);
+        assertEquals(new Tasks().ifTask(5,2),2);
 
     }
 
     @Test(expected = AssertionError.class)
     public void WrongInputIfTask(){
-        assertEquals(new Tasks().IfTask(1,1),0);
+        assertEquals(new Tasks().ifTask(1,1),0);
     }
     @Test
     public void testSwitchTask() {
@@ -68,42 +68,42 @@ public class TasksTest {
         //3 actual = weight  / 1 000
         //4 actual = weight * 1 000
         //5 actual = weight * 100
-        assertEquals(new Tasks().SwitchTask(1,50),50,0.1);
-        assertEquals(new Tasks().SwitchTask(2,1000),0.001,0.1);
-        assertEquals(new Tasks().SwitchTask(3,500),0.5,0.01);
-        assertEquals(new Tasks().SwitchTask(4,5),5000,0.1);
-        assertEquals(new Tasks().SwitchTask(5,30),3000,0.1);
-        assertEquals(new Tasks().SwitchTask(203,30),0,0.1);
+        assertEquals(new Tasks().switchTask(1,50),50,0.1);
+        assertEquals(new Tasks().switchTask(2,1000),0.001,0.1);
+        assertEquals(new Tasks().switchTask(3,500),0.5,0.01);
+        assertEquals(new Tasks().switchTask(4,5),5000,0.1);
+        assertEquals(new Tasks().switchTask(5,30),3000,0.1);
+        assertEquals(new Tasks().switchTask(203,30),0,0.1);
 
     }
 
     @Test
     public void testForTask() {
         //sum of numbers from a to b
-        assertEquals(new Tasks().ForTask(1,100),5050);
-        assertEquals(new Tasks().ForTask(3,7),25);
+        assertEquals(new Tasks().forTask(1,100),5050);
+        assertEquals(new Tasks().forTask(3,7),25);
 
     }
 
     @Test(expected = AssertionError.class)
     public void WrongInputForTask() {
-        assertEquals(new Tasks().ForTask(0,0),9);
-        assertEquals(new Tasks().ForTask(12,5),15);
+        assertEquals(new Tasks().forTask(0,0),9);
+        assertEquals(new Tasks().forTask(12,5),15);
 
     }
 
     @Test
     public void testWhileTask() {
         //Find the smallest k^2 > N
-        assertEquals(new Tasks().WhileTask(10),4);
-        assertEquals(new Tasks().WhileTask(8),3);
+        assertEquals(new Tasks().whileTask(10),4);
+        assertEquals(new Tasks().whileTask(8),3);
 
     }
 
     @Test(expected = AssertionError.class)
     public void WrongInputWhileTask() {
-        assertEquals(new Tasks().WhileTask(-2),4);
-        assertEquals(new Tasks().WhileTask(0),3);
+        assertEquals(new Tasks().whileTask(-2),4);
+        assertEquals(new Tasks().whileTask(0),3);
 
     }
 
@@ -111,9 +111,9 @@ public class TasksTest {
     public void testArrayTask() {
         //find the difference of the arithmetic progression
         int array[] = {1,4,5,7,9};
-        assertEquals(new Tasks().ArrayTask(new int[]{1,3,4}),0);
-        assertEquals(new Tasks().ArrayTask(new int[]{3,5,7}),2);
-        assertEquals(new Tasks().ArrayTask(new int[]{8,11,14,17}),3);
+        assertEquals(new Tasks().arrayTask(new int[]{1,3,4}),0);
+        assertEquals(new Tasks().arrayTask(new int[]{3,5,7}),2);
+        assertEquals(new Tasks().arrayTask(new int[]{8,11,14,17}),3);
 
     }
 
@@ -129,12 +129,12 @@ public class TasksTest {
                 {8,16,7,15},
                 {2,10,1,9},
                 {4,14,3,13}};
-        Assert.assertTrue(Arrays.deepEquals(new Tasks().MatrixTask(mas,4,4),mas2));
+        Assert.assertTrue(Arrays.deepEquals(new Tasks().matrixTask(mas),mas2));
         mas = new int[][]{{-11, -12, -3, -4},
                 {-15, -16, -7, -8},
                 {-9, -10, -1, -2},
                 {-13, -14, -3, -4}};
-        Assert.assertTrue(Arrays.deepEquals(new Tasks().MatrixTask(mas,4,4),mas));
+        Assert.assertTrue(Arrays.deepEquals(new Tasks().matrixTask(mas),mas));
     }
 
 }
